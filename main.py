6dd9a45da941996f66aad11bd6e77e96adb381a6
VieiraEnzo <vieira.enzof@gmail.com>
2022-09-17 13:33:07 -0300

    Novas Funções adicionadas

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
-def amigo():
-    print('amizade')
-    
-amigo()
+agenda = {}
 
-for i in range(5):
-    amigo()
+
+def adicionar_contato():
+    nome = input("Qual o  nome do contato: ")
+    email = input("Qual o email do contato: ")
+    telefone = input("Qual o telefone do contato: ")
+    agenda[nome] = [email,telefone]
+    print(agenda)
+    pass
+
+def deletar_contato():
+    nome = input('Quem vai rodar? ')
+    del agenda(nome)
+    pass
+
+def buscar_contato():
+    pass
+
+while (True):
+
+    resposta = input('Voce quer (i)nserir (b)uscar ou (d)eletrar um contato ou (s)air? ')
+    if(resposta == 'i'):
+        adicionar_contato()
+
+    elif(resposta == 'b'):
+        buscar_contato()
+
+    elif(resposta == 'd'):
+        deletar_contato()
+    
+    elif(resposta == 's'):
+        break
+
+    else:
+        print("Escreve direito caramba")
+
+
+
