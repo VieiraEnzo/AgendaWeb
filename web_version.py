e4c5f233beb34dfa89794313fb067ba4068c4169
VieiraEnzo <vieira.enzof@gmail.com>
2022-09-18 10:21:23 -0300

    Novas Mudanças

--- web_version.py
+++ web_version.py
@@ -4,26 +4,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("main.html")
+    return render_template("index.html")
 
 
 @app.route("/inserir")
-def hello_world():
+def inserirfunc():
     return render_template("inserir.html")
 
 
 @app.route("/deletar")
-def hello_world():
+def deletarfunc():
     return render_template("deletar.html")
 
 
 @app.route("/buscar")
-def hello_world():
+def buscarfunc():
     return render_template("buscar.html")
 
 
 @app.route("/erro")
-def hello_world():
+def errofunc():
     return render_template("erro.html")
 
 
