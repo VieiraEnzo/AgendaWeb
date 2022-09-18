bc257e5a635a1382c7417ddce21cd3b7e3fd7a33
VieiraEnzo <vieira.enzof@gmail.com>
2022-09-17 23:25:26 -0300

    Criando novos templates de telas

--- web_version.py
+++ web_version.py
@@ -4,6 +4,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("teste.html")
+    return render_template("main.html")
+
+
+@app.route("/inserir")
+def hello_world():
+    return render_template("inserir.html")
+
+
+@app.route("/deletar")
+def hello_world():
+    return render_template("deletar.html")
+
+
+@app.route("/buscar")
+def hello_world():
+    return render_template("buscar.html")
+
+
+@app.route("/erro")
+def hello_world():
+    return render_template("erro.html")
+
 
 app.run()
